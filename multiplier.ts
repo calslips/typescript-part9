9bfb0d13b8b0f61b8ec3c1a4297eb3e41567e395
Caleb Phillips <csphillips5@gmail.com>
2021-11-22 16:05:23 -0500

    refactor multiplier to work with command line parameters

--- multiplier.ts
+++ multiplier.ts
@@ -1,6 +1,11 @@
-// const multiplicator = (a: number, b: number, printText: string) => {
-//   console.log(printText, a * b);
-// }
+const multiplicator = (a: number, b: number, printText: string) => {
+  console.log(printText, a * b);
+}
+
+const a: number = Number(process.argv[2])
+const b: number = Number(process.argv[3])
+
+multiplicator(a, b, `Multiplied ${a} and ${b}, the result is:`);
 
 // multiplicator(2, 4, 'The result of multiplying 2 and 4 is:');
 
@@ -9,31 +14,3 @@
 // }
 
 // multiplicator('how about a string?', 4, 'Multiplied a string and 4, the result is:');
-
-type Operation = 'multiply' | 'add' | 'divide';
-
-type Result = number;
-
-const calculator = (a: number, b: number, op: Operation): Result => {
-  switch(op) {
-    case 'multiply':
-      return a * b;
-    case 'add':
-      return a + b;
-    case 'divide':
-      if (b === 0) throw new Error('Can\'t divide by 0!');
-      return a / b;
-    default:
-      throw new Error('Operation is not multiply, add or divide!');
-  }
-}
-
-try {
-  console.log(calculator(3, 6, 'multiply'))
-} catch (error: unknown) {
-  let errorMessage = 'Something went wrong.'
-  if (error instanceof Error) {
-    errorMessage += ' Error: ' + error.message;
-  }
-  console.log(errorMessage);
-}
